da73512600cb78e91ef0d4ca893c2f328915818a
skooter500 <skooter500@gmail.com>
2019-04-11 12:34:52 +0100

    initilai version

---
+++ Assets/Gravity.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravity : MonoBehaviour
+{
+    public float gravity = -9.8f;
+    public float speed = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+
+        if (pos.y > 0)
+        {
+            speed += gravity * Time.deltaTime;
+            pos.y += speed * Time.deltaTime;
+            transform.position = pos;
+            Debug.Log(Time.time);
+        }
+    }
+}
---
+++ Assets/TankController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    [Range(0, 100)]
+    public float speed = 5.0f;
+
+    [Range(0, 360)]
+    public float rotSpeed = 360.0f;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(Time.time);
+        //transform.Translate(0, 0, speed * Time.deltaTime);
+
+        transform.Translate(0, 0, speed * Time.deltaTime * Input.GetAxis("Vertical"));
+        transform.Rotate(0, rotSpeed * Time.deltaTime * Input.GetAxis("Horizontal"), 0);
+
+    }
+}
